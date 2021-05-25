f62d56a0d666ac309fb873c7d08324f298c28de5
Bobby Tables <career@mytaxi.com>
2021-05-25 07:05:50 +0000

    Initital code.

---
+++ src/test/java/com/freenow/FreeNowServerApplicantTestApplicationTests.java
@@ -0,0 +1,18 @@
+package com.freenow;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = FreeNowServerApplicantTestApplication.class)
+public class FreeNowServerApplicantTestApplicationTests
+{
+
+    @Test
+    public void contextLoads()
+    {
+    }
+
+}
